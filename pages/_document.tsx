2740fdf7a7b2b912b2dec8f81c734449808d10b3
ASchwad <a.schoenenwald@gmx.de>
2026-02-14 12:13:14 +0100

    feat(meta): add proper favicon support for all platforms
    Generate multi-size favicons from lighthouse logo for browser tabs,
    iOS bookmarks, and Android home screen. Add web manifest for PWA support.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,11 @@ export default function Document() {
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
-        <link rel="icon" href="/images/logo.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
